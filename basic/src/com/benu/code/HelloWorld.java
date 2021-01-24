0f03c8b72a805df45439c9e834c48922187e7683
mailt <mailt@SACHIN-PC>
2021-01-24 15:17:07 +0530

    6 commit

--- basic/src/com/benu/code/HelloWorld.java
+++ basic/src/com/benu/code/HelloWorld.java
@@ -4,10 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("This is second update at Github website");
-		System.out.println("This is 3rd update @ local m/c");
-		System.out.println("This is 4rth change @ Github");
-		System.out.println("This is 5th change @ Github");
+		System.out.println("This is 1Branch @Local m/c");
 
 	}
 
